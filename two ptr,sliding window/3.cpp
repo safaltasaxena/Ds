ce046f6f293691e995bc408bcb6bf483f072fa9e
safaltasaxena <2405377@kiit.ac.in>
2026-05-05 14:47:04 +0530

    files

--- two ptr,sliding window/3.cpp
+++
@@ -1,4 +0,0 @@
-//no of subarrays w some cnd 
-//gonna use 2. module method here
-//Q-figure out number of subarrays with sum k
-//approach=>ANSWER=(no. of subarrays where sum<=k)-(no of subarrays where sum<=k-1)
--- two ptr,sliding window/4.cpp
+++
@@ -1,2 +0,0 @@
-//shortest/minm window cnd
-//approach=get the valid window then keep on shrinking until u get the smallest window then iterate again for further iterations
