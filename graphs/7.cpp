bf0232f9fd9d1d7498b27a52c2f3d0ccd93044e9
safaltasaxena <2405377@kiit.ac.in>
2026-06-03 22:46:10 +0530

    kruskal

--- graphs/7.cpp
+++ graphs/7.cpp
@@ -27,6 +27,7 @@ class graph{
      }
 };
 
+//PRIM'S ALGORITHM
 void MST_Prim(unordered_map<int, vector<pair<int,int>>> &adj, int start, int n) {
     vector <int> parent(n, -1); // to store the parent of each node in the MST
     vector <int> key(n, INT_MAX); // to store the minimum weight edge for each node
@@ -95,6 +96,65 @@ void MST_Prim(unordered_map<int, vector<pair<int,int>>> &adj, int start, int n) 
     }
 }
 
+int find(int u, vector<int> &parent) {
+    if (parent[u] == u) {
+        return u;
+    }
+    parent[u] = find(parent[u], parent); // Path compression
+    return parent[u];
+}
+
+void union_sets(int u, int v, vector<int> &parent, vector<int> &rank) {
+    int root_u = find(u, parent);
+    int root_v = find(v, parent);
+
+    if (root_u != root_v) {
+        // Union by rank
+        if (rank[root_u] > rank[root_v]) {
+            parent[root_v] = root_u;
+        } else if (rank[root_u] < rank[root_v]) {
+            parent[root_u] = root_v;
+        } else {
+            parent[root_v] = root_u;
+            rank[root_u]++;
+        }
+    }
+}
+
+//KRUSKAL'S ALGORITHM
+void MST_Kruskal(unordered_map<int, vector<pair<int,int>>> &adj, int n) {
+    // Create a list of edges from the adjacency list
+    vector<tuple<int, int, int>> edges; // {weight, u, v}
+    for (auto &entry : adj) {
+        int u = entry.first;
+        for (auto &nbr : entry.second) {
+            int v = nbr.first;
+            int wt = nbr.second;
+            if (u < v) { // to avoid duplicate edges in undirected graph
+                edges.push_back({wt, u, v});
+            }
+        }
+    }
+
+    // Sort edges based on weight
+    sort(edges.begin(), edges.end());
+
+    // Disjoint Set Union (DSU) for cycle detection
+    vector<int> parent(n), rank(n, 0);
+    for (int i = 0; i < n; i++) parent[i] = i;
+
+    cout << "Edge \tWeight" << endl;
+    for (auto &edge : edges) {
+        int wt, u, v;
+        tie(wt, u, v) = edge;
+
+        if (find(u, parent) != find(v, parent)) { // If u and v are in different sets
+            union_sets(u, v, parent, rank); // Union the sets
+            cout << u << " - " << v << "\t" << wt << endl; // Print the edge in MST
+        }
+    }
+}
+
 int main () {
     int n;
     cout<<"Enter the number of nodes"<<endl;
@@ -115,11 +175,11 @@ int main () {
         g.addEdge(u,v,weight);
     }
 
-    int start;
-    cout<<"Enter the starting node for Prim's algorithm"<<endl;
-    cin>>start;
-    MST_Prim(g.adj, start, n);
+    cout<<"MST using Prim's algorithm"<<endl;
+    MST_Prim(g.adj, 0, n);
 
+    cout<<"MST using Kruskal's algorithm"<<endl;
+    MST_Kruskal(g.adj, n);
 
     return 0;
 
