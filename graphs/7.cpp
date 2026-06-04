288d44a460d726c0340fb6c417425a78c8561bf7
safaltasaxena <2405377@kiit.ac.in>
2026-06-04 14:48:12 +0530

    disjoint set

--- graphs/7.cpp
+++ graphs/7.cpp
@@ -184,3 +184,47 @@ int main () {
     return 0;
 
 }
+
+/*
+class DisjointSet {
+public:
+    vector<int> parent, rank;
+
+    // Constructor
+    DisjointSet(int n) {
+        parent.resize(n);
+        rank.resize(n, 0);
+
+        for (int i = 0; i < n; i++) {
+            parent[i] = i; // each node is its own parent initially
+        }
+    }
+
+    // Find with path compression
+    int findUPar(int node) {
+        if (node == parent[node])
+            return node;
+
+        return parent[node] = findUPar(parent[node]);
+    }
+
+    // Union by rank
+    void unionByRank(int u, int v) {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+
+        if (ulp_u == ulp_v) return;
+
+        if (rank[ulp_u] < rank[ulp_v]) {
+            parent[ulp_u] = ulp_v;
+        }
+        else if (rank[ulp_v] < rank[ulp_u]) {
+            parent[ulp_v] = ulp_u;
+        }
+        else {
+            parent[ulp_v] = ulp_u;
+            rank[ulp_u]++;
+        }
+    }
+};
+*/
